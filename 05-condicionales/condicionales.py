17d3a69f64c58b97f62ff8486c3dfdb525a3c6b5
kawazack <59796809+kawazack@users.noreply.github.com>
2021-05-17 13:56:24 +0200

    Add files via upload

---
+++ 05-condicionales/condicionales.py
@@ -0,0 +1,10 @@
+
+
+color = input("Guess what is my favourite color: ")
+
+if color == "red" or "Red":
+
+    print("Enhorabuena! El color es correcto")
+
+else: 
+    print("Lo siento. El color es incorrecto! Inténtalo de nuevo :)")
