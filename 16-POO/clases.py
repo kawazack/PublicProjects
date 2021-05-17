92c0a32aacc9212b813ac1a6c40706b1cbd19888
kawazack <59796809+kawazack@users.noreply.github.com>
2021-05-17 14:00:49 +0200

    Python: Object Oriented Programming exercises

---
+++ 16-POO/clases.py
@@ -0,0 +1,21 @@
+
+
+class persona:
+
+    #atributos o propiedades (variables)
+
+    color = "Rojo"
+    marca = "Ferrari"
+    modelo "Aventador"
+    velocidad = 300
+    caballaje = 500
+    sound = "sound3"
+    plazas = 2
+
+
+#métodos, actions that the object make. (functions)
+
+    def speedup(self):
+        self.speedup += 1
+
+
